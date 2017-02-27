6bc3ea5ec36d56647ce78e88d0ed6efa51a6e4c3
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-02-27 14:36:53 +0100

    my projected use case test

--- XlsxReaderWriterTests/SwiftTestCase.swift
+++ XlsxReaderWriterTests/SwiftTestCase.swift
@@ -9,17 +9,29 @@
 import UIKit
 import XCTest
 
+enum CellValue {
+    case string(String)
+    case date(Date)
+    case integer(Int)
+    case float(Float)
+}
+
+extension BRACell {
+    func setValue(_ value: CellValue) {
+        switch value {
+        case let .string(value):
+            self.setStringValue(value)
+        case let .date(value):
+            self.setDateValue(value)
+        case let .integer(value):
+            self.setIntegerValue(value)
+        case let .float(value):
+            self.setFloatValue(value)
+        }
+    }
+}
+
 class SwiftTestCase: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
 
     func testSwiftOpenClose() {
         // This is an example of a functional test case.
@@ -40,4 +52,65 @@ class SwiftTestCase: XCTestCase {
         XCTAssert(FileManager.default.fileExists(atPath: fullPath), "No file exists at \(fullPath)")
     }
 
+    func testMyUseCase() {
+        let documentPath = Bundle(for: self.classForCoder).path(forResource: "testWorkbook", ofType: "xlsx")
+        let odp: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(documentPath)
+        guard let worksheet = odp.workbook.worksheets.first as? BRAWorksheet else {
+            XCTFail()
+            return
+        }
+
+        var templateCells = [String: BRACell]()
+
+        for cell in worksheet.cells {
+            guard let cell = cell as? BRACell,
+                let strValue = cell.stringValue(),
+                strValue.hasPrefix("#"),
+                strValue.hasSuffix("#")
+                else {
+                    continue
+            }
+            print("cell: \(cell)")
+            print("str: \(strValue)")
+            templateCells[strValue] = cell
+        }
+//        for row in worksheet.rows {
+//            guard let row = row as? BRARow else {
+//                continue
+//            }
+//            for cell in row.cells {
+//                guard let cell = cell as? BRACell,
+//                    let strValue = cell.stringValue(),
+//                    strValue.hasPrefix("#"),
+//                    strValue.hasSuffix("#")
+//                else {
+//                    continue
+//                }
+//                print("cell: \(cell)")
+//                print("str: \(strValue)")
+//                templateCells[strValue] = cell
+//            }
+//        }
+
+        let mapping: [String: CellValue] = [
+            "#CLIENT#": .string("The client"),
+            "#DATE#": .date(Date()),
+            "#COMPTE#": .float(15.12),
+            "#PRIX#": .integer(42)
+        ]
+
+        for templateKey in templateCells.keys {
+            let cell = templateCells[templateKey]!
+            let injectedValue = mapping[templateKey] ?? .string("")
+            cell.setValue(injectedValue)
+        }
+
+        guard let documentsDirPath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first else {
+            XCTFail()
+            return
+        }
+        let fullPath = (documentsDirPath as NSString).appendingPathComponent("testSwiftOpenClose.xlsx")
+        NSLog("\(fullPath)")
+        odp.save(as: fullPath)
+    }
 }
